4d324068995c0eaed9a9fd0d715525b219dbbe11
Akshay Kalapgar <64259085+Akkikens@users.noreply.github.com>
2024-05-31 17:03:05 -0400

    Akshay - Homepage Topbar Optimized it for Mobile View
    Homepage Topbar Optimized it for Mobile View

--- factorbook-2.0/src/components/Topbar.tsx
+++ factorbook-2.0/src/components/Topbar.tsx
@@ -7,40 +7,155 @@ import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
+import HomeIcon from "@mui/icons-material/Home";
+import MenuBookIcon from "@mui/icons-material/MenuBook";
 import Drawer from "@mui/material/Drawer";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
+import Collapse from "@mui/material/Collapse";
 import Divider from "@mui/material/Divider";
+import ExpandLess from "@mui/icons-material/ExpandLess";
+import ExpandMore from "@mui/icons-material/ExpandMore";
+import Grid from "@mui/material/Grid"; // Grid version 2
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { useTheme } from "@mui/material/styles";
 import styles from "./Topbar.module.css";
 
 const navItems = [
-  { title: "Home", href: "#" },
-  { title: "About", href: "#" },
-  { title: "Resources", href: "#" },
+  { title: "Home", href: "#", icon: <HomeIcon sx={{ color: "#8169BF" }} /> },
+  {
+    title: "Portals",
+    icon: <ExpandMore sx={{ color: "#8169BF" }} />,
+    subItems: [
+      { title: "Human Transcription Factors", href: "#" },
+      { title: "Mouse Transcription Factors", href: "#" },
+      { title: "Motif Site Catalog", href: "#" },
+      { title: "Annotate Variants & Trait Heritability", href: "#" },
+    ],
+  },
+  {
+    title: "Resources",
+    href: "#",
+    icon: <MenuBookIcon sx={{ color: "#8169BF" }} />,
+  },
   { title: "Download", href: "#" },
 ];
 
 const Topbar: React.FC = () => {
   const [mobileOpen, setMobileOpen] = React.useState(false);
+  const [portalsOpen, setPortalsOpen] = React.useState(true);
+
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
 
   const handleDrawerToggle = () => {
     setMobileOpen((prevState) => !prevState);
   };
 
+  const handlePortalsClick = () => {
+    setPortalsOpen(!portalsOpen);
+  };
+
   const drawer = (
-    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
-      <Typography variant="h6" sx={{ my: 2 }}>
-        factor book
-      </Typography>
+    <Box sx={{ textAlign: "center", color: "#8169BF", height: "100%" }}>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          padding: "16px",
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "flex-start",
+            textDecoration: "none",
+            color: "var(--grey-700, #1F2021)",
+            fontFamily: "Helvetica Neue",
+            fontSize: "32px",
+            fontStyle: "normal",
+            fontWeight: 500,
+            lineHeight: "0.745", // 74.5%
+            letterSpacing: "-1.28px",
+          }}
+        >
+          <Typography
+            variant="h6"
+            component="div"
+            className={styles.logo}
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "flex-start",
+              fontSize: "32px",
+              fontStyle: "normal",
+              fontWeight: 500,
+              lineHeight: "0.745", // 74.5%
+              letterSpacing: "-1.28px",
+              color: "var(--grey-700, #1F2021)",
+            }}
+          >
+            factor
+            <span className={styles.book}>book</span>
+          </Typography>
+        </Box>
+        <IconButton
+          edge="start"
+          color="inherit"
+          onClick={handleDrawerToggle}
+          aria-label="close"
+        >
+          <CloseIcon />
+        </IconButton>
+      </Box>
       <Divider />
       <List>
-        {navItems.map((item) => (
-          <ListItem key={item.title} disablePadding>
-            <ListItem button component="a" href={item.href}>
-              <ListItemText primary={item.title} />
-            </ListItem>
-          </ListItem>
+        {navItems.map((item, index) => (
+          <React.Fragment key={item.title}>
+            {item.subItems ? (
+              <>
+                <ListItem button onClick={handlePortalsClick}>
+                  <ListItemText primary={item.title} />
+                  {portalsOpen ? <ExpandLess /> : <ExpandMore />}
+                </ListItem>
+                <Collapse in={portalsOpen} timeout="auto" unmountOnExit>
+                  <List component="div" disablePadding>
+                    {item.subItems.map((subItem) => (
+                      <React.Fragment key={subItem.title}>
+                        <ListItem button component="a" href={subItem.href}>
+                          <ListItemText
+                            primary={subItem.title}
+                            sx={{ paddingLeft: "16px" }}
+                          />
+                        </ListItem>
+                        <Divider sx={{ margin: "0 16px" }} />
+                      </React.Fragment>
+                    ))}
+                  </List>
+                </Collapse>
+              </>
+            ) : (
+              <ListItem button component="a" href={item.href}>
+                <ListItemText primary={item.title} />
+                {item.icon && (
+                  <Box
+                    sx={{
+                      display: "flex",
+                      alignItems: "center",
+                      paddingLeft: "8px",
+                    }}
+                  >
+                    {item.icon}
+                  </Box>
+                )}
+              </ListItem>
+            )}
+            {index < navItems.length - 1 && <Divider />}
+          </React.Fragment>
         ))}
       </List>
     </Box>
@@ -53,74 +168,136 @@ const Topbar: React.FC = () => {
       sx={{ backgroundColor: "transparent", boxShadow: "none" }}
     >
       <Toolbar disableGutters className={styles.toolbar}>
-        <Box
-          component="a"
-          href="#"
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "flex-start",
-            textDecoration: "none",
-            color: "var(--grey-700, #1F2021)",
-            fontFamily: "Helvetica Neue",
-            fontSize: "32px",
-            fontStyle: "normal",
-            fontWeight: 500,
-            lineHeight: "0.745" /* 74.5% */,
-            letterSpacing: "-1.28px",
-          }}
-        >
-          <Typography
-            variant="h6"
-            component="div"
-            className={styles.logo}
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "flex-start",
-              // fontFamily: "Helvetica Neue",
-              fontSize: "32px",
-              fontStyle: "normal",
-              fontWeight: 500,
-              lineHeight: "0.745" /* 74.5% */,
-              letterSpacing: "-1.28px",
-              color: "var(--grey-700, #1F2021)",
-            }}
-          >
-            factor
-            <span className={styles.book}>book</span>
-          </Typography>
-        </Box>
-        <Box
-          sx={{
-            flexGrow: 1,
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            gap: "20px",
-          }}
-        >
-          {navItems.map((item) => (
-            <Button
-              key={item.title}
-              href={item.href}
-              className={styles.navItem}
-              sx={{
-                // fontFamily: "Helvetica Neue",
-                fontSize: "15px",
-                fontStyle: "normal",
-                fontWeight: 500,
-                lineHeight: "26px" /* 173.333% */,
-                letterSpacing: "0.46px",
-                color: "var(--primary-mainText, #6750A4)",
-                fontFeatureSettings: "'clig' off, 'liga' off",
-              }}
-            >
-              {item.title}
-            </Button>
-          ))}
-        </Box>
-        <Box sx={{ display: { xs: "none", md: "flex" } }}></Box>
+        <Grid container alignItems="center">
+          {isMobile ? (
+            <>
+              <Grid item xs={6}>
+                <IconButton
+                  edge="start"
+                  color="inherit"
+                  aria-label="menu"
+                  onClick={handleDrawerToggle}
+                >
+                  <MenuIcon sx={{ color: "grey" }} />
+                </IconButton>
+              </Grid>
+              <Grid
+                item
+                xs={6}
+                sx={{ display: "flex", justifyContent: "flex-end" }}
+              >
+                <Box
+                  component="a"
+                  href="#"
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "flex-start",
+                    textDecoration: "none",
+                    color: "var(--grey-700, #1F2021)",
+                    fontFamily: "Helvetica Neue",
+                    fontSize: "32px",
+                    fontStyle: "normal",
+                    fontWeight: 500,
+                    lineHeight: "0.745", // 74.5%
+                    letterSpacing: "-1.28px",
+                  }}
+                >
+                  <Typography
+                    variant="h6"
+                    component="div"
+                    className={styles.logo}
+                    sx={{
+                      display: "flex",
+                      flexDirection: "column",
+                      alignItems: "flex-start",
+                      fontSize: "32px",
+                      fontStyle: "normal",
+                      fontWeight: 500,
+                      lineHeight: "0.745", // 74.5%
+                      letterSpacing: "-1.28px",
+                      color: "var(--grey-700, #1F2021)",
+                    }}
+                  >
+                    factor
+                    <span className={styles.book}>book</span>
+                  </Typography>
+                </Box>
+              </Grid>
+            </>
+          ) : (
+            <>
+              <Grid item xs="auto">
+                <Box
+                  component="a"
+                  href="#"
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "flex-start",
+                    textDecoration: "none",
+                    color: "var(--grey-700, #1F2021)",
+                    fontFamily: "Helvetica Neue",
+                    fontSize: "32px",
+                    fontStyle: "normal",
+                    fontWeight: 500,
+                    lineHeight: "0.745", // 74.5%
+                    letterSpacing: "-1.28px",
+                  }}
+                >
+                  <Typography
+                    variant="h6"
+                    component="div"
+                    className={styles.logo}
+                    sx={{
+                      display: "flex",
+                      flexDirection: "column",
+                      alignItems: "flex-start",
+                      fontSize: "32px",
+                      fontStyle: "normal",
+                      fontWeight: 500,
+                      lineHeight: "0.745", // 74.5%
+                      letterSpacing: "-1.28px",
+                      color: "var(--grey-700, #1F2021)",
+                    }}
+                  >
+                    factor
+                    <span className={styles.book}>book</span>
+                  </Typography>
+                </Box>
+              </Grid>
+              <Grid item xs>
+                <Box
+                  sx={{
+                    display: "flex",
+                    justifyContent: "flex-end",
+                    width: "100%",
+                  }}
+                >
+                  {navItems.map((item) => (
+                    <Button
+                      key={item.title}
+                      href={item.href}
+                      className={styles.navItem}
+                      sx={{
+                        fontSize: "15px",
+                        fontStyle: "normal",
+                        fontWeight: 500,
+                        lineHeight: "26px", // 173.333%
+                        letterSpacing: "0.46px",
+                        color: "var(--primary-mainText, #6750A4)",
+                        fontFeatureSettings: "'clig' off, 'liga' off",
+                        marginLeft: "16px",
+                      }}
+                    >
+                      {item.title}
+                    </Button>
+                  ))}
+                </Box>
+              </Grid>
+            </>
+          )}
+        </Grid>
       </Toolbar>
       <nav>
         <Drawer
@@ -129,6 +306,11 @@ const Topbar: React.FC = () => {
           onClose={handleDrawerToggle}
           ModalProps={{
             keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            "& .MuiDrawer-paper": {
+              width: "100%",
+            },
           }}
         >
           {drawer}
