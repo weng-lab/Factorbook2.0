5b71e8c9e93effd920d4af334b1a2496a40273e6
jpfisher72 <jpfisher72@gmail.com>
2024-06-03 12:44:20 -0400

    Empty config, rm next-transpile-modules

--- factorbook-2.0/next.config.mjs
+++ factorbook-2.0/next.config.mjs
@@ -1,23 +1,28 @@
-import withTM from 'next-transpile-modules';
-import path from 'path';
-import { fileURLToPath } from 'url';
+// import withTM from 'next-transpile-modules';
+// import path from 'path';
+// import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
-const withTranspileModules = withTM(['@mui/material', '@mui/system', '@mui/icons-material']);
+// const withTranspileModules = withTM(['@mui/material', '@mui/system', '@mui/icons-material']);
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true, // Enable SWC minification
+//   webpack: (config) => {
+//     config.resolve.alias = {
+//       ...config.resolve.alias,
+//       '@emotion/react': path.resolve(__dirname, 'node_modules/@emotion/react'),
+//     };
+//     return config;
+//   },
+// };
+
+// export default withTranspileModules(nextConfig);
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true, // Enable SWC minification
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@emotion/react': path.resolve(__dirname, 'node_modules/@emotion/react'),
-    };
-    return config;
-  },
-};
+const nextConfig = {};
 
-export default withTranspileModules(nextConfig);
+export default nextConfig;
