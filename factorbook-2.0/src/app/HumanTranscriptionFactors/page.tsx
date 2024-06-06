0f6f1f5d6dcf0edad6c2c3c08e8df255266250b9
Akshay Kalapgar <64259085+Akkikens@users.noreply.github.com>
2024-06-06 15:36:35 -0400

    [Akshay] Avoided Repetitive Code

--- factorbook-2.0/src/app/HumanTranscriptionFactors/page.tsx
+++ factorbook-2.0/src/app/HumanTranscriptionFactors/page.tsx
@@ -1,218 +1,31 @@
-"use client";
-
 import * as React from "react";
-import { Box, Typography, useMediaQuery, Tabs, Tab } from "@mui/material";
-import { useTheme } from "@mui/material/styles";
-import Grid2 from "@mui/material/Unstable_Grid2"; // Grid version 2
-import Searchbar from "@/components/Searchbar";
+import TranscriptionFactors from "@/components/TranscriptionFactors";
 
 const HumanTranscriptionFactors = () => {
-  const theme = useTheme();
-  const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
-  const isMediumScreen = useMediaQuery(theme.breakpoints.between("sm", "md"));
-  const [tabValue, setTabValue] = React.useState(0);
-
-  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
-    setTabValue(newValue);
-  };
+  const humanContent = `
+    Transcription factors (TFs) are regulatory proteins in the
+    complex networks that underpin cellular function. They bind to
+    specific DNA sequences, typically in the regulatory regions of
+    the genome. They activate or repress the transcription of genes,
+    thereby controlling the flow of genetic information from DNA to
+    mRNA. The human genome encodes for approximately 1800 TFs, each
+    with unique binding sites and mechanisms of action. TFs are
+    often categorized based on their DNA binding domains and the
+    sequences they recognize. Their activity is regulated by various
+    mechanisms, including post-translational modifications,
+    interaction with other proteins, and environmental signals. TFs
+    are central to many biological processes, such as development,
+    cell cycle, and response to stimuli. Dysregulation of TF
+    activity can lead to a variety of diseases, including cancer,
+    making them significant targets in biomedical research.
+  `;
 
   return (
-    <>
-      <Box
-        sx={{
-          backgroundColor: "#2A2A2D",
-          width: "100%",
-          minHeight: "700px",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          overflow: "hidden",
-          paddingTop: "40px",
-          position: "relative",
-          margin: "0",
-          boxSizing: "border-box",
-        }}
-      >
-        <Box
-          sx={{
-            width: "100%",
-            maxWidth: "1440px",
-            minHeight: "700px",
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
-            flexShrink: 0,
-            padding: "0 24px",
-            gap: "24px",
-            boxSizing: "border-box",
-            margin: "0",
-            position: "relative",
-            zIndex: 1,
-          }}
-        >
-          <Grid2
-            container
-            spacing={2}
-            sx={{
-              width: "100%",
-              alignItems: "center",
-              flexShrink: 0,
-              boxSizing: "border-box",
-              margin: 0,
-            }}
-          >
-            <Grid2
-              xs={12}
-              md={6}
-              sx={{
-                textAlign: isSmallScreen ? "center" : "left",
-                padding: isSmallScreen ? "0 10px" : "0",
-                boxSizing: "border-box",
-                order: isSmallScreen ? 2 : 1,
-              }}
-            >
-              <Typography
-                variant="h3"
-                sx={{
-                  color: "var(--common-white-main, #FFF)",
-                  fontFeatureSettings: "'clig' off, 'liga' off",
-                  fontSize: "48px",
-                  fontStyle: "normal",
-                  fontWeight: 400,
-                  lineHeight: "116.7%",
-                  marginBottom: "20px",
-                  textAlign: isSmallScreen ? "center" : "left",
-                }}
-              >
-                Human Transcription Factors
-              </Typography>
-              <Typography
-                variant="body1"
-                sx={{
-                  color: "var(--common-white-main, #FFF)",
-                  fontFeatureSettings: "'clig' off, 'liga' off",
-                  fontSize: "16px",
-                  fontStyle: "normal",
-                  fontWeight: 400,
-                  lineHeight: "150%",
-                  letterSpacing: "0.15px",
-                  maxWidth: "900px",
-                  marginBottom: "20px",
-                  textAlign: isSmallScreen ? "center" : "left",
-                }}
-              >
-                Transcription factors (TFs) are regulatory proteins in the
-                complex networks that underpin cellular function. They bind to
-                specific DNA sequences, typically in the regulatory regions of
-                the genome. They activate or repress the transcription of genes,
-                thereby controlling the flow of genetic information from DNA to
-                mRNA. The human genome encodes for approximately 1800 TFs, each
-                with unique binding sites and mechanisms of action. TFs are
-                often categorized based on their DNA binding domains and the
-                sequences they recognize. Their activity is regulated by various
-                mechanisms, including post-translational modifications,
-                interaction with other proteins, and environmental signals. TFs
-                are central to many biological processes, such as development,
-                cell cycle, and response to stimuli. Dysregulation of TF
-                activity can lead to a variety of diseases, including cancer,
-                making them significant targets in biomedical research.
-              </Typography>
-              <Searchbar
-                placeholder="What are you searching for today?"
-                helperText=""
-              />
-            </Grid2>
-            <Grid2
-              xs={12}
-              md={6}
-              sx={{
-                display: isSmallScreen ? "none" : "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                flexShrink: 0,
-                padding: "0",
-                boxSizing: "border-box",
-                order: 2,
-                position: "relative",
-                zIndex: 1,
-              }}
-            >
-              <img
-                src="/Face.png"
-                alt="Illustration"
-                style={{
-                  width: "100%",
-                  maxWidth: isMediumScreen ? "80%" : "80%",
-                  height: "auto",
-                  flexShrink: 0,
-                  objectFit: "contain",
-                }}
-              />
-            </Grid2>
-          </Grid2>
-        </Box>
-        {isSmallScreen && (
-          <Box
-            sx={{
-              position: "absolute",
-              top: 0,
-              left: 0,
-              width: "100%",
-              height: "100%",
-              zIndex: 0,
-              overflow: "hidden",
-            }}
-          >
-            <img
-              src="/Face.png"
-              alt="Illustration"
-              style={{
-                width: "100%",
-                height: "100%",
-                objectFit: "cover",
-                opacity: 0.5,
-              }}
-            />
-          </Box>
-        )}
-      </Box>
-
-      <Box
-        sx={{
-          width: "100%",
-          maxWidth: "1440px",
-          margin: "0 auto",
-          padding: "0 24px",
-          boxSizing: "border-box",
-        }}
-      >
-        <Tabs
-          value={tabValue}
-          onChange={handleChange}
-          textColor="primary"
-          indicatorColor="primary"
-          aria-label="primary tabs example"
-          sx={{ marginBottom: "20px" }}
-        >
-          <Tab
-            label="Browse all Transcription Factors"
-            sx={{ textTransform: "none" }}
-          />
-          <Tab label="Browse all Cell Types" sx={{ textTransform: "none" }} />
-        </Tabs>
-        <Box>
-          {tabValue === 0 && (
-            <Typography>
-              Content for Browse all Transcription Factors
-            </Typography>
-          )}
-          {tabValue === 1 && (
-            <Typography>Content for Browse all Cell Types</Typography>
-          )}
-        </Box>
-      </Box>
-    </>
+    <TranscriptionFactors
+      header="Human Transcription Factors"
+      content={humanContent}
+      image="/Face.png"
+    />
   );
 };
 
---
+++ factorbook-2.0/src/components/TranscriptionFactors.tsx
@@ -0,0 +1,213 @@
+"use client";
+
+import * as React from "react";
+import { Box, Typography, useMediaQuery, Tabs, Tab } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+import Grid2 from "@mui/material/Unstable_Grid2"; // Grid version 2
+import Searchbar from "@/components/Searchbar";
+
+const TranscriptionFactors = ({
+  header,
+  content,
+  image,
+}: {
+  header: string;
+  content: string;
+  image: string;
+}) => {
+  const theme = useTheme();
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
+  const isMediumScreen = useMediaQuery(theme.breakpoints.between("sm", "md"));
+  const [tabValue, setTabValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setTabValue(newValue);
+  };
+
+  return (
+    <>
+      <Box
+        sx={{
+          backgroundColor: "#2A2A2D",
+          width: "100%",
+          minHeight: "700px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          overflow: "hidden",
+          paddingTop: "40px",
+          position: "relative",
+          margin: "0",
+          boxSizing: "border-box",
+        }}
+      >
+        <Box
+          sx={{
+            width: "100%",
+            maxWidth: "1440px",
+            minHeight: "700px",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            flexShrink: 0,
+            padding: "0 24px",
+            gap: "24px",
+            boxSizing: "border-box",
+            margin: "0",
+            position: "relative",
+            zIndex: 1,
+          }}
+        >
+          <Grid2
+            container
+            spacing={2}
+            sx={{
+              width: "100%",
+              alignItems: "center",
+              flexShrink: 0,
+              boxSizing: "border-box",
+              margin: 0,
+            }}
+          >
+            <Grid2
+              xs={12}
+              md={6}
+              sx={{
+                textAlign: isSmallScreen ? "center" : "left",
+                padding: isSmallScreen ? "0 10px" : "0",
+                boxSizing: "border-box",
+                order: isSmallScreen ? 2 : 1,
+              }}
+            >
+              <Typography
+                variant="h3"
+                sx={{
+                  color: "var(--common-white-main, #FFF)",
+                  fontFeatureSettings: "'clig' off, 'liga' off",
+                  fontSize: "48px",
+                  fontStyle: "normal",
+                  fontWeight: 400,
+                  lineHeight: "116.7%",
+                  marginBottom: "20px",
+                  textAlign: isSmallScreen ? "center" : "left",
+                }}
+              >
+                {header}
+              </Typography>
+              <Typography
+                variant="body1"
+                sx={{
+                  color: "var(--common-white-main, #FFF)",
+                  fontFeatureSettings: "'clig' off, 'liga' off",
+                  fontSize: "16px",
+                  fontStyle: "normal",
+                  fontWeight: 400,
+                  lineHeight: "150%",
+                  letterSpacing: "0.15px",
+                  maxWidth: "900px",
+                  marginBottom: "20px",
+                  textAlign: isSmallScreen ? "center" : "left",
+                }}
+              >
+                {content}
+              </Typography>
+              <Searchbar
+                placeholder="What are you searching for today?"
+                helperText=""
+              />
+            </Grid2>
+            <Grid2
+              xs={12}
+              md={6}
+              sx={{
+                display: isSmallScreen ? "none" : "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                flexShrink: 0,
+                padding: "0",
+                boxSizing: "border-box",
+                order: 2,
+                position: "relative",
+                zIndex: 1,
+              }}
+            >
+              <img
+                src={image}
+                alt="Illustration"
+                style={{
+                  width: "100%",
+                  maxWidth: isMediumScreen ? "80%" : "100%",
+                  height: "auto",
+                  flexShrink: 0,
+                  objectFit: "contain",
+                }}
+              />
+            </Grid2>
+          </Grid2>
+        </Box>
+        {isSmallScreen && (
+          <Box
+            sx={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              width: "100%",
+              height: "100%",
+              zIndex: 0,
+              overflow: "hidden",
+            }}
+          >
+            <img
+              src={image}
+              alt="Illustration"
+              style={{
+                width: "100%",
+                height: "100%",
+                objectFit: "cover",
+                opacity: 0.5,
+              }}
+            />
+          </Box>
+        )}
+      </Box>
+
+      <Box
+        sx={{
+          width: "100%",
+          maxWidth: "1440px",
+          margin: "0 auto",
+          padding: "0 24px",
+          boxSizing: "border-box",
+        }}
+      >
+        <Tabs
+          value={tabValue}
+          onChange={handleChange}
+          textColor="primary"
+          indicatorColor="primary"
+          aria-label="primary tabs example"
+          sx={{ marginBottom: "20px" }}
+        >
+          <Tab
+            label="Browse all Transcription Factors"
+            sx={{ textTransform: "none" }}
+          />
+          <Tab label="Browse all Cell Types" sx={{ textTransform: "none" }} />
+        </Tabs>
+        <Box>
+          {tabValue === 0 && (
+            <Typography>
+              Content for Browse all Transcription Factors
+            </Typography>
+          )}
+          {tabValue === 1 && (
+            <Typography>Content for Browse all Cell Types</Typography>
+          )}
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+export default TranscriptionFactors;
