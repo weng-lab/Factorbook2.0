437c4b3bdc440949d51df7aecaada1e88a0ecefd
Akshay Kalapgar <64259085+Akkikens@users.noreply.github.com>
2024-06-11 15:58:21 -0400

    [Akshay] Bug Fixes
    Bug Fixes

--- src/app/AnnotationsVariants/page.tsx
+++ src/app/AnnotationsVariants/page.tsx
@@ -60,15 +60,15 @@ const AnnotationsVariants = () => {
     setSubpopulation(event.target.value as string);
   };
 
-  const motifsContent = `
+  const annotationsContent = `
   Genetic variants in regulatory elements of the human genome play a critical role in influencing traits and disease susceptibility by modifying transcription factor (TF) binding and gene expression. Often identified in genome-wide association studies, these variants can disrupt gene regulatory networks, leading to varied phenotypic effects or predispositions to diseases. Factorbook offers a comprehensive resource of TF binding motifs and sites, enabling researchers to predict the impact of genetic variants on TF binding and gene regulation, providing valuable insights into the functional consequences of these variants.
   `;
 
   return (
     <>
       <TranscriptionFactors
-        header="Motifs Site Catlog"
-        content={motifsContent}
+        header="Annotations"
+        content={annotationsContent}
         image="/Human.png"
       />
       <Box sx={{ width: "100%", bgcolor: "background.paper", mt: 4 }}>
