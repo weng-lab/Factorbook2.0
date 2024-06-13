cd2220de8a8d7a807cb7307d71ddcce0ee6a773c
Akshay Kalapgar <64259085+Akkikens@users.noreply.github.com>
2024-06-13 14:50:45 -0400

    [Akshay] ApolloClient Installation and Initial Setup

---
+++ lib/client.ts
@@ -0,0 +1,19 @@
+import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client"
+import { registerApolloClient } from "@apollo/experimental-nextjs-app-support";
+import Config from "../src/config.json"
+/**
+ * @returns an ApolloClient instance scoped for the current request
+ */
+
+export const { getClient } = registerApolloClient(() => {
+  return new ApolloClient({
+    ssrMode: true,
+    cache: new InMemoryCache(),
+    link: new HttpLink({
+      uri: Config.API.CcreAPI,
+      // you can disable result caching here if you want to
+      // (this does not work if you are rendering your page with `export const dynamic = "force-static"`)
+      fetchOptions: { cache: "no-store" },
+    }),
+  })
+})
