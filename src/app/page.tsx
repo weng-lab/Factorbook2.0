985abab9f63b9411157980b7b05574e0fb30a4d3
Jonathan Fisher <49597360+jpfisher72@users.noreply.github.com>
2024-06-17 13:53:20 -0400

    Generate types from GQL schema (#8)
    * Generate types from schema, move config file
    
    * Add dummy query to fix build - remove once any other query is typed with new gql function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,7 @@ import Searchbar from "@/components/Searchbar";
 import Header from "@/components/Header";
 import SelectComponent from "@/components/Select";
 import PortalPanel from "@/components/PortalPanel";
+import { gql } from "../types/gql"
 
 const Homepage = () => {
   const theme = useTheme();
@@ -42,6 +43,17 @@ const Homepage = () => {
       router.push("/MouseTranscriptionFactors");
     }
   };
+
+  /**
+   * @todo Remove this once any other query is typed. This is a useless query needed to prevent build errors with graphql-code-generator
+   */
+  const QUERY = gql(`
+    query LDSC($study: [String]){
+      iCRELdrQuery(study: $study) {
+        snps
+      }
+    }`
+  )
 
   return (
     <>
