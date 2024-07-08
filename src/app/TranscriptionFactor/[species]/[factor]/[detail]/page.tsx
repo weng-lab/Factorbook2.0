56c65a78e84fca0dc86212209a8f9c77bc0a3540
Akshay Kalapgar <64259085+Akkikens@users.noreply.github.com>
2024-07-08 10:19:00 -0400

    [Akshay] Minor updates

--- src/app/TranscriptionFactor/[species]/[factor]/[detail]/page.tsx
+++ src/app/TranscriptionFactor/[species]/[factor]/[detail]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import { useRouter, usePathname } from "next/navigation";
+import { useRouter, useParams } from "next/navigation";
 import { useQuery } from "@apollo/client";
 import {
   Box,
@@ -12,26 +12,23 @@ import {
   Tabs,
   Tab,
 } from "@mui/material";
+import SaveAltIcon from "@mui/icons-material/SaveAlt";
 import { FACTOR_DESCRIPTION_QUERY } from "@/components/tf/Query";
 import { FactorQueryResponse } from "@/components/CellType/types";
-import SaveAltIcon from "@mui/icons-material/SaveAlt";
 import { getRCSBImageUrl } from "@/components/tf/Functions";
 import ContentCard from "@/components/ContentCard";
 import ReferenceSection from "@/components/Container";
-
-import FunctionTab from "./Function";
-import Expression from "./Expression";
-import MotifEnrichmentMEME from "./MotifEnrichmentMEME";
-import MotifEnrichmentSELEX from "./MotifEnrichmentSELEX";
-import EpigeneticProfile from "./EpigeneticProfile";
+import FunctionTab from "@/app/TranscriptionFactor/[species]/[factor]/[detail]/Function";
+import Expression from "@/app/TranscriptionFactor/[species]/[factor]/[detail]/Expression";
+import MotifEnrichmentMEME from "@/app/TranscriptionFactor/[species]/[factor]/[detail]/MotifEnrichmentMEME";
+import MotifEnrichmentSELEX from "@/app/TranscriptionFactor/[species]/[factor]/[detail]/MotifEnrichmentSELEX";
+import EpigeneticProfile from "@/app/TranscriptionFactor/[species]/[factor]/[detail]/EpigeneticProfile";
+import Search from "@/app/TranscriptionFactor/[species]/[factor]/[detail]/Search";
+import Link from "next/link";
 
 const FactorDetailsPage = () => {
   const router = useRouter();
-  const pathname = usePathname();
-  const segments = pathname.split("/");
-  const species = segments[2];
-  const factor = segments[3];
-  const detail = segments[4] || "Function";
+  const { species, factor, detail = "Function" } = useParams();
 
   const { data, loading, error } = useQuery<FactorQueryResponse>(
     FACTOR_DESCRIPTION_QUERY,
@@ -61,10 +58,6 @@ const FactorDetailsPage = () => {
     { name: "Wikipedia", url: "https://www.wikipedia.org/" },
   ];
 
-  const handleTabChange = (event: React.SyntheticEvent, newValue: string) => {
-    router.push(`/TranscriptionFactor/${species}/${factor}/${newValue}`);
-  };
-
   const renderTabContent = () => {
     switch (detail) {
       case "Function":
@@ -77,6 +70,8 @@ const FactorDetailsPage = () => {
         return <MotifEnrichmentSELEX />;
       case "EpigeneticProfile":
         return <EpigeneticProfile />;
+      case "Search":
+        return <Search />;
       default:
         return <FunctionTab />;
     }
@@ -84,26 +79,30 @@ const FactorDetailsPage = () => {
 
   return (
     <Container>
-      <Box display="flex" justifyContent="space-between" mb={2}>
+      <Box display="flex" alignItems="center" mb={2}>
         {imageUrl && (
           <Box>
             <img
               src={imageUrl}
               alt={factorData?.name}
-              style={{ maxWidth: "300px" }}
+              style={{ width: "150px", marginRight: "20px" }}
             />
           </Box>
         )}
-        <Box style={{ flex: 1, marginLeft: "20px" }}>
-          <Typography variant="h4">{factorData?.name}</Typography>
-          <Typography variant="h6">Function</Typography>
+        <Box>
+          <Typography variant="h4" style={{ color: "#494A50" }}>
+            {factorData?.name}
+          </Typography>
+          <Typography variant="h6" style={{ color: "#494A50" }}>
+            {detail}
+          </Typography>
           <Typography variant="body1">{factorData?.factor_wiki}</Typography>
           <IconButton
             onClick={() => {
               const tsvData = [
                 "image\tlabel\tname\tdescription",
                 `${imageUrl || ""}\t${factorData?.name}\t${
-                  factorData?.factor_wiki || ""
+                  factorData?.description || ""
                 }`,
               ].join("\n");
               const dataStr = `data:text/tab-separated-values;charset=utf-8,${encodeURIComponent(
@@ -125,45 +124,72 @@ const FactorDetailsPage = () => {
           </IconButton>
         </Box>
       </Box>
-      <ContentCard
-        title={factorData?.name || ""}
-        count={0}
-        description={factorData?.factor_wiki || ""}
-      />
-      <ReferenceSection title="References" sources={references} />
-      <Tabs value={detail} onChange={handleTabChange}>
-        <Tab
-          label="Function"
-          value="Function"
-          component="a"
-          href={`/TranscriptionFactor/${species}/${factor}/Function`}
-        />
-        <Tab
-          label="Expression (RNA-seq)"
-          value="Expression"
-          component="a"
-          href={`/TranscriptionFactor/${species}/${factor}/Expression`}
-        />
-        <Tab
-          label="Motif Enrichment (MEME, ChIP-seq)"
-          value="MotifEnrichmentMEME"
-          component="a"
-          href={`/TranscriptionFactor/${species}/${factor}/MotifEnrichmentMEME`}
-        />
-        <Tab
-          label="Motif Enrichment (SELEX)"
-          value="MotifEnrichmentSELEX"
-          component="a"
-          href={`/TranscriptionFactor/${species}/${factor}/MotifEnrichmentSELEX`}
-        />
-        <Tab
-          label="Epigenetic Profile"
-          value="EpigeneticProfile"
-          component="a"
-          href={`/TranscriptionFactor/${species}/${factor}/EpigeneticProfile`}
-        />
-      </Tabs>
-      <Box mt={4}>{renderTabContent()}</Box>
+      <Box mb={2}>
+        <Link href="/">Homepage</Link> &gt;{" "}
+        <Link href={`/TranscriptionFactor/${species}`}>
+          Transcription Factor
+        </Link>{" "}
+        &gt; <Typography component="span">{factor}</Typography>
+      </Box>
+      <Box display="flex">
+        <Box
+          style={{
+            backgroundColor: "var(--grey-500, #494A50)",
+            padding: "16px",
+            borderRadius: "8px",
+            marginRight: "20px",
+          }}
+        >
+          <ReferenceSection title="References" sources={references} />
+        </Box>
+        <Box style={{ flex: 1 }}>
+          <Tabs
+            value={detail}
+            indicatorColor="primary"
+            textColor="primary"
+            variant="scrollable"
+            scrollButtons="auto"
+          >
+            <Tab
+              label="Function"
+              value="Function"
+              component={Link}
+              href={`/TranscriptionFactor/${species}/${factor}/Function`}
+            />
+            <Tab
+              label="Expression (RNA-seq)"
+              value="Expression"
+              component={Link}
+              href={`/TranscriptionFactor/${species}/${factor}/Expression`}
+            />
+            <Tab
+              label="Motif Enrichment (MEME, ChIP-seq)"
+              value="MotifEnrichmentMEME"
+              component={Link}
+              href={`/TranscriptionFactor/${species}/${factor}/MotifEnrichmentMEME`}
+            />
+            <Tab
+              label="Motif Enrichment (SELEX)"
+              value="MotifEnrichmentSELEX"
+              component={Link}
+              href={`/TranscriptionFactor/${species}/${factor}/MotifEnrichmentSELEX`}
+            />
+            <Tab
+              label="Epigenetic Profile"
+              value="EpigeneticProfile"
+              component={Link}
+              href={`/TranscriptionFactor/${species}/${factor}/EpigeneticProfile`}
+            />
+            <Tab
+              label={`Search ${factor} peaks by region`}
+              value="Search"
+              component={Link}
+              href={`/TranscriptionFactor/${species}/${factor}/Search`}
+            />
+          </Tabs>
+          <Box mt={2}>{renderTabContent()}</Box>
+        </Box>
+      </Box>
     </Container>
   );
 };
