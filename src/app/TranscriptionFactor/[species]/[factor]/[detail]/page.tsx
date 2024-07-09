e1069e07958a8191c5b59f297fc1e55ba6450d2b
Akshay Kalapgar <64259085+Akkikens@users.noreply.github.com>
2024-07-09 01:33:33 -0400

    Updated the whole components

--- src/app/TranscriptionFactor/[species]/[factor]/[detail]/page.tsx
+++ src/app/TranscriptionFactor/[species]/[factor]/[detail]/page.tsx
@@ -6,14 +6,17 @@ import { useQuery } from "@apollo/client";
 import {
   Box,
   CircularProgress,
-  Container,
   Typography,
   IconButton,
   Tabs,
   Tab,
+  useMediaQuery,
+  useTheme,
 } from "@mui/material";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import ArrowDropUpIcon from "@mui/icons-material/ArrowDropUp";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import { FACTOR_DESCRIPTION_QUERY } from "@/components/tf/Query";
 import { FactorQueryResponse } from "@/components/CellType/types";
 import { getRCSBImageUrl } from "@/components/tf/Functions";
@@ -30,6 +33,9 @@ const FactorDetailsPage = () => {
   const router = useRouter();
   const { species, factor, detail = "Function" } = useParams();
   const [imageVisible, setImageVisible] = useState(true);
+
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
 
   const { data, loading, error } = useQuery<FactorQueryResponse>(
     FACTOR_DESCRIPTION_QUERY,
@@ -82,7 +88,8 @@ const FactorDetailsPage = () => {
     <Box
       style={{
         display: "flex",
-        padding: "40px 25.5px",
+        flexDirection: isMobile ? "column" : "row",
+        padding: isMobile ? "20px 10px" : "40px 25.5px",
         minHeight: "100vh",
       }}
     >
@@ -94,9 +101,9 @@ const FactorDetailsPage = () => {
           background: "var(--grey-500, #494A50)",
           padding: "16px",
           borderRadius: "8px",
-          marginRight: "20px",
-          width: "300px",
-          minHeight: "calc(100vh - 80px)", // Adjust height to fill the viewport without overlapping header/footer
+          marginRight: isMobile ? "0" : "20px",
+          width: isMobile ? "100%" : "300px",
+          minHeight: isMobile ? "auto" : "calc(100vh - 80px)", // Adjust height to fill the viewport without overlapping header/footer
         }}
       >
         <Typography
@@ -110,18 +117,29 @@ const FactorDetailsPage = () => {
             <img
               src={imageUrl}
               alt={factorData?.name}
-              style={{ width: "200px", marginBottom: "20px" }}
+              style={{
+                width: isMobile ? "100%" : "200px",
+                marginBottom: "20px",
+              }}
             />
             <IconButton
               onClick={() => setImageVisible(!imageVisible)}
-              style={{ position: "absolute", top: "0", right: "-30px" }}
+              style={{
+                position: "absolute",
+                top: "0",
+                right: "-40px",
+                color: "white",
+              }}
             >
               <ArrowDropUpIcon />
             </IconButton>
           </Box>
         )}
         {!imageVisible && (
-          <IconButton onClick={() => setImageVisible(!imageVisible)}>
+          <IconButton
+            onClick={() => setImageVisible(!imageVisible)}
+            style={{ color: "white" }}
+          >
             <ArrowDropDownIcon />
           </IconButton>
         )}
@@ -137,50 +155,88 @@ const FactorDetailsPage = () => {
           &gt; <Typography component="span">{factor}</Typography>
         </Box>
 
-        <Tabs
-          value={detail}
-          indicatorColor="primary"
-          textColor="primary"
-          variant="scrollable"
-          scrollButtons="auto"
-        >
-          <Tab
-            label="Function"
-            value="Function"
-            component={Link}
-            href={`/TranscriptionFactor/${species}/${factor}/Function`}
-          />
-          <Tab
-            label="Expression (RNA-seq)"
-            value="Expression"
-            component={Link}
-            href={`/TranscriptionFactor/${species}/${factor}/Expression`}
-          />
-          <Tab
-            label="Motif Enrichment (MEME, ChIP-seq)"
-            value="MotifEnrichmentMEME"
-            component={Link}
-            href={`/TranscriptionFactor/${species}/${factor}/MotifEnrichmentMEME`}
-          />
-          <Tab
-            label="Motif Enrichment (SELEX)"
-            value="MotifEnrichmentSELEX"
-            component={Link}
-            href={`/TranscriptionFactor/${species}/${factor}/MotifEnrichmentSELEX`}
-          />
-          <Tab
-            label="Epigenetic Profile"
-            value="EpigeneticProfile"
-            component={Link}
-            href={`/TranscriptionFactor/${species}/${factor}/EpigeneticProfile`}
-          />
-          <Tab
-            label={`Search ${factor} peaks by region`}
-            value="Search"
-            component={Link}
-            href={`/TranscriptionFactor/${species}/${factor}/Search`}
-          />
-        </Tabs>
+        <Box display="flex" alignItems="center">
+          <ArrowBackIosIcon
+            onClick={() => {
+              const tabsScroller = document.querySelector(".MuiTabs-scroller");
+              if (tabsScroller) {
+                tabsScroller.scrollBy({ left: -200, behavior: "smooth" });
+              }
+            }}
+          ></ArrowBackIosIcon>
+          <Tabs
+            value={detail}
+            indicatorColor="primary"
+            textColor="primary"
+            variant="scrollable"
+            scrollButtons="auto"
+          >
+            <Tab
+              label="Function"
+              value="Function"
+              component={Link}
+              href={`/TranscriptionFactor/${species}/${factor}/Function`}
+              sx={{
+                color: detail === "Function" ? "#8169BF" : "inherit",
+              }}
+            />
+            <Tab
+              label="Expression (RNA-seq)"
+              value="Expression"
+              component={Link}
+              href={`/TranscriptionFactor/${species}/${factor}/Expression`}
+              sx={{
+                color: detail === "Expression" ? "#8169BF" : "inherit",
+              }}
+            />
+            <Tab
+              label="Motif Enrichment (MEME, ChIP-seq)"
+              value="MotifEnrichmentMEME"
+              component={Link}
+              href={`/TranscriptionFactor/${species}/${factor}/MotifEnrichmentMEME`}
+              sx={{
+                color: detail === "MotifEnrichmentMEME" ? "#8169BF" : "inherit",
+              }}
+            />
+            <Tab
+              label="Motif Enrichment (SELEX)"
+              value="MotifEnrichmentSELEX"
+              component={Link}
+              href={`/TranscriptionFactor/${species}/${factor}/MotifEnrichmentSELEX`}
+              sx={{
+                color:
+                  detail === "MotifEnrichmentSELEX" ? "#8169BF" : "inherit",
+              }}
+            />
+            <Tab
+              label="Epigenetic Profile"
+              value="EpigeneticProfile"
+              component={Link}
+              href={`/TranscriptionFactor/${species}/${factor}/EpigeneticProfile`}
+              sx={{
+                color: detail === "EpigeneticProfile" ? "#8169BF" : "inherit",
+              }}
+            />
+            <Tab
+              label={`Search ${factor} peaks by region`}
+              value="Search"
+              component={Link}
+              href={`/TranscriptionFactor/${species}/${factor}/Search`}
+              sx={{
+                color: detail === "Search" ? "#8169BF" : "inherit",
+              }}
+            />
+          </Tabs>
+          <ArrowForwardIosIcon
+            onClick={() => {
+              const tabsScroller = document.querySelector(".MuiTabs-scroller");
+              if (tabsScroller) {
+                tabsScroller.scrollBy({ left: 200, behavior: "smooth" });
+              }
+            }}
+          ></ArrowForwardIosIcon>
+        </Box>
+
         <Box mt={2}>{renderTabContent()}</Box>
       </Box>
     </Box>
