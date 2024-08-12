d42c89dcf84e82adbf6b1ef18904aecb46f665f6
Akshay Kalapgar <64259085+Akkikens@users.noreply.github.com>
2024-08-12 10:05:07 -0400

    [Akshay] Added Styling

--- src/app/TranscriptionFactor/[species]/[factor]/[detail]/GeneExpression.tsx
+++ src/app/TranscriptionFactor/[species]/[factor]/[detail]/GeneExpression.tsx
@@ -11,18 +11,18 @@ import {
   Button,
   Grid,
 } from "@mui/material";
-import { groupBy } from 'queryz';
+import { groupBy } from "queryz";
 import { Download as DownloadIcon } from "@mui/icons-material";
 import { formatFactorName } from "@/utilities/misc";
 import { useGeneExpressionData } from "@/components/tf/geneexpression/hooks";
 import { GeneExpressionPageProps } from "@/components/tf/geneexpression/types";
-import { ViolinPlotMousedOverState } from './violin/types';
+import { ViolinPlotMousedOverState } from "./violin/types";
 import {
   downloadTSV,
   downloadSVG,
   spacedColors,
 } from "@/components/tf/geneexpression/utils";
-import ViolinPlot from './violin/violin';
+import ViolinPlot from "./violin/violin";
 import styled from "@emotion/styled";
 
 export const StyledButton = styled(Button)(() => ({
@@ -49,97 +49,104 @@ type Quantification = {
 };
 
 const GeneExpressionPage: React.FC<GeneExpressionPageProps> = (props) => {
-  console.log(props,"ge props")
   const [polyA, setPolyA] = useState(false);
   const { data, loading } = useGeneExpressionData(
     props.assembly,
-    formatFactorName(
-      props.gene_name,
-      props.assembly
-    ),
+    formatFactorName(props.gene_name, props.assembly),
     polyA ? "polyA plus RNA-seq" : "total RNA-seq"
   );
-  console.log("data",data)
-  const [mousedOver, setMousedOver] = useState<ViolinPlotMousedOverState>({ inner: null, outer: null });
+  const [mousedOver, setMousedOver] = useState<ViolinPlotMousedOverState>({
+    inner: null,
+    outer: null,
+  });
   const biosampleTypes = new Set(
     data?.gene_dataset.map((x: GeneDataset) => x.biosample_type) || []
   );
   const [biosampleType, setBiosampleType] = useState(2);
   const sortedBiosampleTypes = useMemo(
-    () => [...biosampleTypes].sort().filter(x => x !== 'in vitro differentiated cells'),
+    () =>
+      [...biosampleTypes]
+        .sort()
+        .filter((x) => x !== "in vitro differentiated cells"),
     [biosampleTypes]
-);
-const ref = useRef<SVGSVGElement>(null);
+  );
+  const ref = useRef<SVGSVGElement>(null);
 
-const grouped = useMemo(
+  const grouped = useMemo(
     () =>
-        groupBy(
-            data?.gene_dataset.filter(x => x.gene_quantification_files.length > 0) || [],
-            x => x.biosample_type,
-            x => x
-        ),
+      groupBy(
+        data?.gene_dataset.filter(
+          (x) => x.gene_quantification_files.length > 0
+        ) || [],
+        (x) => x.biosample_type,
+        (x) => x
+      ),
     [data]
-);
-const subGrouped = useMemo(
+  );
+  const subGrouped = useMemo(
     () =>
-        groupBy(
-            grouped.get(sortedBiosampleTypes[biosampleType]) || [],
-            x => (sortedBiosampleTypes[biosampleType] === 'tissue' ? x.tissue : x.biosample),
-            x => x
-        ),
+      groupBy(
+        grouped.get(sortedBiosampleTypes[biosampleType]) || [],
+        (x) =>
+          sortedBiosampleTypes[biosampleType] === "tissue"
+            ? x.tissue
+            : x.biosample,
+        (x) => x
+      ),
     [grouped, sortedBiosampleTypes, biosampleType]
-);
-const sortedKeys = useMemo(
+  );
+  const sortedKeys = useMemo(
     () =>
-        [...subGrouped.keys()]
-            .filter(
-                x =>
-                    x !== null &&
-                    subGrouped
-                        .get(x)!
-                        .flatMap(x => x.gene_quantification_files)
-                        .filter(x => x.quantifications[0]?.tpm !== undefined).length > 2
-            )
-            .sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase())),
+      [...subGrouped.keys()]
+        .filter(
+          (x) =>
+            x !== null &&
+            subGrouped
+              .get(x)!
+              .flatMap((x) => x.gene_quantification_files)
+              .filter((x) => x.quantifications[0]?.tpm !== undefined).length > 2
+        )
+        .sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase())),
     [subGrouped]
-);
-const toPlot = useMemo(
+  );
+  const toPlot = useMemo(
     () =>
-        new Map(
-            sortedKeys
-                .map(
-                    x =>
-                        [
-                            x,
-                            new Map([
-                                [
-                                    'all',
-                                    subGrouped
-                                        .get(x)!
-                                        .flatMap(x =>
-                                            x.gene_quantification_files.map(x => x.quantifications[0]?.tpm)
-                                        )
-                                        .filter(x => x !== undefined)
-                                        .map(x => Math.log10(x! + 0.01)),
-                                ],
-                            ]),
-                        ] as [string, Map<string, number[]>]
-                )
-                .filter(x => x[1].get('all')!.length > 1)
-        ),
+      new Map(
+        sortedKeys
+          .map(
+            (x) =>
+              [
+                x,
+                new Map([
+                  [
+                    "all",
+                    subGrouped
+                      .get(x)!
+                      .flatMap((x) =>
+                        x.gene_quantification_files.map(
+                          (x) => x.quantifications[0]?.tpm
+                        )
+                      )
+                      .filter((x) => x !== undefined)
+                      .map((x) => Math.log10(x! + 0.01)),
+                  ],
+                ]),
+              ] as [string, Map<string, number[]>]
+          )
+          .filter((x) => x[1].get("all")!.length > 1)
+      ),
     [sortedKeys, subGrouped]
-);
+  );
 
-console.log("toplot",toPlot)
-const domain: [number, number] = useMemo(() => {
-    const values = [...toPlot.values()].flatMap(x => x.get('all')!);
+  const domain: [number, number] = useMemo(() => {
+    const values = [...toPlot.values()].flatMap((x) => x.get("all")!);
     return [Math.log10(0.01), Math.max(...values, 4.5)];
-}, [toPlot]);
-const width = useMemo(() => {
+  }, [toPlot]);
+  const width = useMemo(() => {
     const keys = [...toPlot.keys()].length;
     return (28 + (keys < 27 ? 27 : keys)) * 200;
-}, [toPlot]);
-const color = useCallback(spacedColors(sortedKeys.length), [sortedKeys]);
+  }, [toPlot]);
+  const color = useCallback(spacedColors(sortedKeys.length), [sortedKeys]);
   const download = useCallback(() => {
     downloadTSV(
       "cell type\ttissue ontology\tbiosample type\texperiment accession\tfile accession\tTPM\n" +
@@ -181,83 +188,97 @@ const color = useCallback(spacedColors(sortedKeys.length), [sortedKeys]);
           <Tab label="Total RNA-seq" value={false} />
         </Tabs>
       </AppBar>
-      <Grid container>
-        <Grid item xs={3}>
-          <Paper>
-            <Typography variant="h6" gutterBottom>
-              Select a biosample type:
-            </Typography>
-            {sortedBiosampleTypes.map((t, i) => (
-              <MenuItem
-                key={t}
-                onClick={() => setBiosampleType(i)}
-                selected={i === biosampleType}
+      <Box sx={{ marginTop: "1em" }}>
+        {" "}
+        {/* Added margin to create a gap beneath the tabs */}
+        <Grid container>
+          <Grid item xs={3}>
+            <Paper sx={{ backgroundColor: "#f5f5f5", padding: "1em" }}>
+              {" "}
+              {/* Added background color and padding */}
+              <Typography variant="subtitle1" gutterBottom>
+                {" "}
+                {/* Adjusted the font size */}
+                Select a biosample type:
+              </Typography>
+              {sortedBiosampleTypes.map((t, i) => (
+                <MenuItem
+                  key={t}
+                  onClick={() => setBiosampleType(i)}
+                  selected={i === biosampleType}
+                >
+                  {t}s
+                </MenuItem>
+              ))}
+            </Paper>
+          </Grid>
+          <Grid item xs={9}>
+            <Paper>
+              <Typography variant="h5" style={{ marginLeft: "5em" }}>
+                {props.gene_name} expression in{" "}
+                {sortedBiosampleTypes[biosampleType]}s: RNA-seq
+              </Typography>
+              <StyledButton
+                variant="contained"
+                color="primary"
+                size="small"
+                startIcon={<DownloadIcon />}
+                style={{ marginLeft: "7.5em" }}
+                onClick={download}
               >
-                {t}s
-              </MenuItem>
-            ))}
-          </Paper>
-        </Grid>
-        <Grid item xs={9}>
-          <Paper>
-            <Typography variant="h5" style={{ marginLeft: "5em" }}>
-              {props.gene_name} expression in{" "}
-              {sortedBiosampleTypes[biosampleType]}s: RNA-seq
-            </Typography>
-            <StyledButton
-              variant="contained"
-              color="primary"
-              size="small"
-              startIcon={<DownloadIcon />}
-              style={{ marginLeft: "7.5em" }}
-              onClick={download}
-            >
-              Download all {polyA ? "poly-A enriched" : "total RNA-seq"}{" "}
-              expression data for {props.gene_name}
-            </StyledButton>
-            <StyledButton
-              variant="contained"
-              color="primary"
-              size="small"
-              startIcon={<DownloadIcon />}
-              style={{ marginLeft: "7.5em" }}
-              onClick={() =>
-                ref.current &&
-                downloadSVG(ref, `${props.gene_name}-gene-expression.svg`)
-              }
-            >
-              Export plot as SVG
-            </StyledButton>
-            {toPlot.size > 0 ? (
-            <svg viewBox={`0 0 ${width} ${width / 2}`} style={{ width: '100%' }} ref={ref}>
-              <ViolinPlot
-                  data={toPlot}
-                  title="log10 TPM"
-                  width={width}
-                  height={width / 2}
-                  colors={new Map(sortedKeys.map((x, i) => [x, color(i)]))}
-                  domain={domain}
-                  tKeys={28}
-                  onViolinMousedOut={() => setMousedOver({ inner: null, outer: null })}
-                  onViolinMousedOver={setMousedOver}
-                  mousedOver={mousedOver}
-              />
-            </svg>
-            ) : (
-              <Paper
-                style={{ marginLeft: "6.5em", width: "70%", padding: "1em" }}
+                Download all {polyA ? "poly-A enriched" : "total RNA-seq"}{" "}
+                expression data for {props.gene_name}
+              </StyledButton>
+              <StyledButton
+                variant="contained"
+                color="primary"
+                size="small"
+                startIcon={<DownloadIcon />}
+                style={{ marginLeft: "7.5em" }}
+                onClick={() =>
+                  ref.current &&
+                  downloadSVG(ref, `${props.gene_name}-gene-expression.svg`)
+                }
               >
-                <Typography variant="body1" color="error">
-                  There is no expression data available for the assay and
-                  biosample combination you have selected. Please use the menus
-                  above and to the left of this message to select a different
-                  combination.
-                </Typography>
-              </Paper>
-            )}
-          </Paper>
+                Export plot as SVG
+              </StyledButton>
+              {toPlot.size > 0 ? (
+                <svg
+                  viewBox={`0 0 ${width} ${width / 2}`}
+                  style={{ width: "100%" }}
+                  ref={ref}
+                >
+                  <ViolinPlot
+                    data={toPlot}
+                    title="log10 TPM"
+                    width={width}
+                    height={width / 2}
+                    colors={new Map(sortedKeys.map((x, i) => [x, color(i)]))}
+                    domain={domain}
+                    tKeys={28}
+                    onViolinMousedOut={() =>
+                      setMousedOver({ inner: null, outer: null })
+                    }
+                    onViolinMousedOver={setMousedOver}
+                    mousedOver={mousedOver}
+                  />
+                </svg>
+              ) : (
+                <Paper
+                  style={{ marginLeft: "6.5em", width: "70%", padding: "1em" }}
+                >
+                  <Typography variant="body1" color="error">
+                    There is no expression data available for the assay and
+                    biosample combination you have selected. Please use the
+                    menus above and to the left of this message to select a
+                    different combination.
+                  </Typography>
+                </Paper>
+              )}
+            </Paper>
+          </Grid>
         </Grid>
-      </Grid>
+      </Box>
     </Box>
   );
 };
