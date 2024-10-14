4e482789a0ceb439dfc16d21b1471a23ebb7ee51
NishiPhalke <nishaphalke@gmail.com>
2024-10-14 16:06:09 -0400

    tf peaks

--- src/app/TranscriptionFactor/[species]/[factor]/[detail]/Search.tsx
+++ src/app/TranscriptionFactor/[species]/[factor]/[detail]/Search.tsx
@@ -1,18 +1,57 @@
 "use client";
 
-import React, { useState } from "react";
-import { Box, Typography, Button, styled } from "@mui/material";
+import React, { useState, SetStateAction, useMemo } from "react";
+import { Pagination, PaginationItem } from "@mui/material";
+import { Box, Typography, Button, styled, useTheme, TextField, Link, Modal } from "@mui/material";
 import DriveFolderUploadIcon from "@mui/icons-material/DriveFolderUpload";
-import Searchbar from "@/components/Searchbar";
+import { useQuery } from "@apollo/client";
 import { useParams } from "next/navigation";
+import { DNALogo } from "logojs-react";
+import {
+  DataTable,
+  DataTableColumn,
+} from "@weng-lab/psychscreen-ui-components";
+import { useMotifsInPeak } from "./hooks";
+import { MotifQueryDataOccurrence, MotifQueryDataOccurrenceMotif, PeakQueryResponse, PeakResult, TomtomMatchQueryData } from "./types";
+import { PEAK_QUERY,  TOMTOM_MATCH_QUERY } from "./Queries";
 
-const StyledBox = styled(Box)({
-  padding: "16px",
-  backgroundColor: "#f9f9f9",
-  borderRadius: "8px",
-  boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
-  marginTop: "16px",
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 1000,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
+
+const StyledSearchBox = styled(Box)({
+  "& .MuiOutlinedInput-root": {
+    backgroundColor: "#EDE7F6",
+  },
 });
+const LargeTextField = styled(TextField)(({ theme }) => ({
+  minWidth: "700px",
+  "& .MuiInputBase-root": {
+    height: "32px",
+  },
+  "& .MuiOutlinedInput-root": {
+    backgroundColor: "#EDE7F6",
+    height: "40px",
+    borderRadius: "24px",
+    paddingLeft: "5px",
+    "&:hover fieldset": {
+      borderColor: theme.palette.primary.main,
+    },
+    "&.Mui-focused fieldset": {
+      borderColor: theme.palette.primary.main,
+    },
+  },
+}));
+
 
 const UploadBox = styled(Box, {
   shouldForwardProp: (prop) => prop !== "isDragging",
@@ -25,9 +64,385 @@ const UploadBox = styled(Box, {
   marginTop: "16px",
 }));
 
+export type GenomicRange = {
+  chromosome?: string;
+  start?: number;
+  end?: number;
+};
+
+
+const PEAKS_COLUMNS = (): DataTableColumn<PeakResult>[] => {
+  const cols: DataTableColumn<PeakResult>[] = [
+      {
+          header: 'Chromosome',
+          value: (row: PeakResult) => row.chrom,
+      },
+      {
+          header: 'Start',
+          value: (row: PeakResult) => row.chrom_start,
+          render: (row: PeakResult) => row.chrom_start.toLocaleString(),
+      },
+      {
+          header: 'End',
+          value: (row: PeakResult) => row.chrom_end,
+          render: (row: PeakResult) => row.chrom_end.toLocaleString(),
+      },
+      {
+          header: 'Biosample',
+          value: (row: PeakResult) => row.biosample,
+      },
+      {
+          header: 'File Accession',
+          value: (row: PeakResult) => row.file_accession,
+          render: (row: PeakResult) => (
+            <Link
+            style={{ color: "#8169BF" }}
+            rel="noopener noreferrer"
+            target="_blank"
+            href={`https://www.encodeproject.org/files/${row.file_accession}/`}
+          >
+            {row.file_accession}
+          </Link>
+            
+          ),
+      },
+      {
+          header: 'q value',
+          value: (row: PeakResult) => row.q_value.toFixed(2),
+      },
+  ];
+  //if (includeFactor) cols.splice(4, 0, { header: 'Factor', value: row => row.target });
+  cols.push({
+      header: 'Motifs found',
+      value: () => '',
+      render: (row: PeakResult) => <MotifCell chromosome={row.chrom} start={row.chrom_start} end={row.chrom_end} />,
+  } as any);
+  return cols;
+};
+
+const MotifCell: React.FC<{ chromosome: string; start: number; end: number }> = ({ chromosome, start, end }) => {
+  const [modalOpen, setModalOpen] = useState(false);
+  const { data: occurrences, status, error } = useMotifsInPeak({ chromosome, start, end });
+  const loading = status === 'pending';
+  return (
+      <div>
+          {loading && <img style={{ width: '16px', height: '16px' }} src={"/loader.svg"} alt={'Loading'} />}
+          {error && <span>Error!</span>}
+          {!loading &&
+              !error &&
+              occurrences &&
+              (occurrences.length > 0 ? (
+                  <button
+                      style={{
+                          background: 'none',
+                          border: 'none',
+                          padding: 0,
+                          fontFamily: 'arial, sans-serif',
+                          color: '#069',
+                          cursor: 'pointer',
+                          outline: 'none',
+                      }}
+                      onClick={() => setModalOpen(true)}
+                  >
+                      {occurrences.length >= 30 ? '>30' : occurrences.length}
+                  </button>
+              ) : (
+                  0
+              ))}
+          {!loading && !error && occurrences && modalOpen && (
+              <>
+               <MotifsModal
+                    open={modalOpen}
+                    setOpen={setModalOpen}
+                    chromosome={chromosome}
+                    start={start}
+                    end={end}
+                    occurrences={occurrences}
+                />
+              </>
+          )}
+      </div>
+  );
+};
+const PWMCell: React.FC<{ peaks_accession: string; motif: MotifQueryDataOccurrenceMotif }> = ({
+  peaks_accession,
+  motif: { pwm, id },
+}) => {
+  const { data, error, loading } = useQuery<TomtomMatchQueryData>(TOMTOM_MATCH_QUERY, {
+      
+      variables: {
+          peaks_accessions: peaks_accession,
+          ids: id,
+      },
+  });
+  let matchLine = <></>
+  if (!loading && !error && data) {
+      const match = data.target_motifs
+          .slice()
+          .filter(x => x.e_value < 1e-5)
+          .sort((a, b) => a.e_value - b.e_value)[0];
+      if (match === undefined) {
+          matchLine = <span>(no external database match)</span>;
+      } else {
+          const jasparName = match.jaspar_name ? `/${match.jaspar_name}` : '';
+          const source = match.target_id.startsWith('MA') ? 'JASPAR' : 'HOCOMOCO';
+          matchLine = <span>{`${match.target_id}${jasparName} (${source})`}</span>;
+      }
+  }
+  return (
+      <>
+          <DNALogo ppm={pwm} height={'50px'} />
+          <br />
+          {matchLine}
+      </>
+  );
+};
+
+const MOTIFS_COLS: DataTableColumn<MotifQueryDataOccurrence>[] = [
+  {
+      header: 'Chromosome',
+      value: row => row.genomic_region.chromosome,
+  },
+  {
+      header: 'Start',
+      value: row => row.genomic_region.start,
+      render: row => row.genomic_region.start.toLocaleString(),
+  },
+  {
+      header: 'Peaks file',
+      value: row => row.peaks_accession,
+  },
+  {
+      header: 'PWM',
+      value: () => '',
+      render: row => <PWMCell peaks_accession={row.peaks_accession} motif={row.motif} />,
+  },
+  {
+      header: 'q value',
+      value: row => row.q_value.toFixed(2),
+  },
+];
+
+
+type MotifsModalProps = {
+  open: boolean;
+  setOpen: (open: boolean) => void;
+  chromosome: string;
+  start: number;
+  end: number;
+  occurrences: MotifQueryDataOccurrence[];
+};
+const MotifsModal: React.FC<MotifsModalProps> = ({
+  open,
+  setOpen,
+  chromosome,
+  start: peakStart,
+  end: peakEnd,
+  occurrences,
+}) => {
+  const motifs = useMemo(
+      () => occurrences.slice().sort((a, b) => a.genomic_region.start - b.genomic_region.start) || [],
+      [occurrences]
+  );
+  const [page, setPage] = useState(1);
+  const width = 500;
+  const height = 25;
+  const start = Math.min(peakStart, motifs[0]?.genomic_region.start || 0);
+  const end = Math.max(peakEnd, motifs[motifs.length - 1]?.genomic_region.end || 0);
+  const rangeSize = end - start;
+  const pageSize = 4;
+  const pageStart = Math.min(motifs.length - 1, (page-1) * pageSize);
+  const pageEnd = Math.min(motifs.length, ((page - 1) + 1) * pageSize);
+  const pageRangeStart = motifs[pageStart]?.genomic_region.start || 0;
+  const pageRangeEnd = motifs[pageEnd - 1]?.genomic_region.end || 0;
+  const pagePxStart = ((pageRangeStart - start) / rangeSize) * width;
+  const pagePxEnd = ((pageRangeEnd - start) / rangeSize) * width;
+  const firstY = height * 0.8;
+  const secondY = firstY + height * 2;
+  
+  const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
+    setPage(value);
+  };
+
+
+  const pageMotifs = motifs.slice(pageStart, pageEnd);
+  const overlaps = (range: [number, number], instance: MotifQueryDataOccurrence) =>
+      instance.genomic_region.start < range[1] && instance.genomic_region.end > range[0];
+  let groupRange: [number, number] | undefined = undefined;
+  let groups: MotifQueryDataOccurrence[][] = [];
+  let group: MotifQueryDataOccurrence[] = [];
+  for (let i = 0; i < pageMotifs.length; i++) {
+      const instance = pageMotifs[i];
+      if (groupRange === undefined) {
+          groupRange = [instance.genomic_region.start, instance.genomic_region.end];
+      }
+      if (overlaps(groupRange, instance)) {
+          group.push(instance);
+          groupRange[1] = instance.genomic_region.end;
+      } else {
+          groups.push(group);
+          group = [instance];
+          groupRange = [instance.genomic_region.start, instance.genomic_region.end];
+      }
+  }
+  groups.push(group);
+  const peakView = (
+      <svg width={width} height={height * 5}>
+          <text textAnchor={'start'} dominantBaseline="hanging" x={0} y={0}>
+              {start.toLocaleString()}
+          </text>
+          <text textAnchor={'end'} dominantBaseline="hanging" x={width} y={0}>
+              {end.toLocaleString()}
+          </text>
+          <rect
+              width={width}
+              height={height}
+              y={firstY}
+              rx="5"
+              style={{
+                  fill: 'grey',
+                  strokeWidth: 3,
+                  stroke: 'none',
+              }}
+          />
+          <g>
+              {motifs.map((instance, i) => {
+                  const motifStart = instance.genomic_region.start - start;
+                  const motifEnd = instance.genomic_region.end - start;
+                  const pxStart = (width * motifStart) / rangeSize;
+                  const pxEnd = (width * motifEnd) / rangeSize;
+                  return (
+                      <rect
+                          key={i}
+                          width={pxEnd - pxStart}
+                          height={height}
+                          x={pxStart}
+                          y={firstY}
+                          style={{ fill: 'red', opacity: '50%' }}
+                      />
+                  );
+              })}
+          </g>
+          <line
+              x1={pagePxStart}
+              x2={0}
+              y1={firstY + height}
+              y2={secondY}
+              style={{ stroke: 'black', strokeWidth: '1px' }}
+          />
+          <line
+              x1={pagePxEnd}
+              x2={width}
+              y1={firstY + height}
+              y2={secondY}
+              style={{ stroke: 'black', strokeWidth: '1px' }}
+          />
+          <rect
+              width={width}
+              height={height}
+              y={secondY}
+              rx="5"
+              style={{
+                  fill: 'grey',
+                  strokeWidth: 3,
+                  stroke: 'none',
+              }}
+          />
+          <g>
+              {groups.flatMap((group, groupi) =>
+                  group.map((instance, i) => {
+                      const rangeSize = pageRangeEnd - pageRangeStart;
+                      const start = instance.genomic_region.start - pageRangeStart;
+                      const end = instance.genomic_region.end - pageRangeStart;
+                      const pxStart = (width * start) / rangeSize;
+                      const pxEnd = (width * end) / rangeSize;
+                      return (
+                          <rect
+                              key={groupi * 100 + i}
+                              x={pxStart}
+                              width={pxEnd - pxStart}
+                              y={secondY + i * (height / group.length)}
+                              height={height / group.length}
+                              rx={5}
+                              style={{
+                                  fill: 'red',
+                                  opacity: '50%',
+                                  stroke: 'darkred',
+                                  strokeWidth: '1px',
+                              }}
+                          />
+                      );
+                  })
+              )}
+          </g>
+          <text textAnchor={'start'} dominantBaseline="baseline" x={0} y={secondY + height * 1.8}>
+              {pageRangeStart.toLocaleString()}
+          </text>
+          <text textAnchor={'end'} dominantBaseline="baseline" x={width} y={secondY + height * 1.8}>
+              {pageRangeEnd.toLocaleString()}
+          </text>
+      </svg>
+  );
+
+  return (
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <>
+          <Typography variant="h6">
+              Motifs found in {chromosome}:{peakStart.toLocaleString()}-{peakEnd.toLocaleString()}
+          </Typography>
+          
+          <Box sx={style}>
+              <div style={{ display: 'flex', justifyContent: 'center' }}>{peakView}</div>
+              {motifs && (
+                  <DataTable
+                      columns={MOTIFS_COLS}
+                      rows={motifs}
+                      sortColumn={1}
+                      key={"sebvibeGFVE"+page}
+                      hidePageMenu
+                      sortDescending
+                      itemsPerPage={pageSize}
+                      page={page-1}
+                     
+                  />
+              )}
+              <br/>
+              {motifs  && (
+        <Pagination
+          sx={{ marginLeft: "450px" }}
+          renderItem={(item) => (
+            <PaginationItem
+              sx={{
+                "&.Mui-selected": {
+                  backgroundColor: "#8169BF",
+                },
+              }}
+              {...item}
+            />
+          )}
+          onChange={handleChange}
+          count={Math.ceil(motifs.length / pageSize)}
+          page={+(page)}
+          color="secondary"
+          showFirstButton
+          showLastButton
+        />
+      )}
+            </Box>
+          </>
+          
+      </Modal>
+  );
+};
+
+
+
 const Search: React.FC = () => {
+  const theme = useTheme();
   const [isDragging, setIsDragging] = useState<boolean>(false);
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const [value, setValue] = useState("")
+  const [regions, setRegions] = useState<GenomicRange[]>([]);
 
   const handleDragOver = (event: React.DragEvent<HTMLDivElement>) => {
     event.preventDefault();
@@ -57,10 +472,31 @@ const Search: React.FC = () => {
     }
   };
 
-  const { factor } = useParams<{
+
+  const { factor, species } = useParams<{
     factor: string;
+    species: string;
   }>();
 
+  const handleChange = (event: { target: { value: SetStateAction<string> } }) => {
+    setValue(event.target.value)
+  }
+  const formattedRegions = useMemo(
+    () => regions.map(x => ({ chrom: x.chromosome!, chrom_start: x.start!, chrom_end: x.end! })),
+    [regions]
+);
+  
+const { data: peaksData, loading, error } = useQuery<PeakQueryResponse>(PEAK_QUERY, {        
+        variables: {
+            assembly: species.toLowerCase() ==="human" ? "GRCh38" : "mm10",
+            range: formattedRegions,
+            target: factor,
+            limit: 1000,
+        },
+        skip: formattedRegions.length === 0
+    });
+
+  console.log("peaksData",peaksData)
   return (
     <Box
       sx={{
@@ -74,12 +510,68 @@ const Search: React.FC = () => {
       <Typography variant="h6" gutterBottom>
         Searching ENCODE ChIP-seq peaks for {factor}
       </Typography>
-      <StyledBox>
-        <Searchbar placeholder="" helperText="" />
-        <Typography variant="body2" sx={{ mt: -2 }}>
-          Example: chr1:10000000-10100000
-        </Typography>
-      </StyledBox>
+      {regions.length==0 && <Box sx={{ mt: 4, mx: "auto", maxWidth: "800px" }}>
+          <Typography variant="h6" gutterBottom>
+          {`Enter genomic coordinates (${species.toLowerCase() ==="human" ? "GRCh38" : "mm10"}):`}
+          </Typography>
+          <StyledSearchBox>
+            <LargeTextField
+              onKeyDown={(event) => {                
+                if (event.key === "Tab" && !value) {
+                  const defaultGenomicRegion = `chr1:${(100000000).toLocaleString()}-${(100101000).toLocaleString()}`
+                  setValue(defaultGenomicRegion)
+                }
+              }}
+              placeholder="Enter a genomic region"
+              onChange={handleChange}
+              id="region-input"
+              
+              value={value}
+            />{" "}
+            <Button
+              variant="contained"
+              sx={{
+                margin: "auto",
+                backgroundColor: theme.palette.primary.main,
+                borderRadius: "24px",
+                textTransform: "none",
+                fontWeight: "medium",
+                color: "#FFFFFF",
+                "&:focus, &:hover, &:active": {
+                  backgroundColor: theme.palette.primary.main,
+                },
+              }}
+              onClick={() => {
+                //console.log(value.split(":")[0],value.split(":")[1].split("-")[0].replaceAll(",",""),value.split(":")[1].split("-")[1].replaceAll(",",""),"val")
+                const chromosome = value.split(":")[0]
+                const start = +value.split(":")[1].split("-")[0].replaceAll(",","")
+                const end = +value.split(":")[1].split("-")[1].replaceAll(",","")
+                setRegions([{ chromosome: chromosome, start: start!!, end: end!! }])
+                
+              }}
+            >
+              Search
+            </Button>
+            <br/>
+            
+            <Typography variant="body2">
+              example: chr1:100,000,000-100,101,000
+            </Typography>
+          </StyledSearchBox>
+      </Box>}
+      {peaksData && peaksData.peaksrange.data &&  <Box sx={{ mt: 4, mx: "auto", maxWidth: "800px" }}><DataTable
+            key="tfpeaks"
+            columns={PEAKS_COLUMNS()}
+            rows={peaksData.peaksrange.data}
+            itemsPerPage={10}
+            searchable
+            tableTitle={`${peaksData.peaksrange.data.length} ${factor} peaks matched your input:`}
+          />
+          </Box>
+         
+
+      }
+      {0>1 && <>
       <Typography variant="h6" gutterBottom>
         You could also upload MEME files here
       </Typography>
@@ -149,6 +641,8 @@ const Search: React.FC = () => {
           Upload File
         </Button>
       </Box>
+      
+      </>}
     </Box>
   );
 };
---
+++ src/app/TranscriptionFactor/[species]/[factor]/[detail]/hooks.ts
@@ -0,0 +1,103 @@
+import { useContext, useEffect, useState, useCallback } from 'react';
+import { MOTIFS_QUERY } from './Queries';
+import { MotifQueryDataOccurrence, MotifQueryData } from './types';
+
+import { ApiContext } from "@/ApiContext";
+type Range = { chromosome: string; start: number; end: number };
+export function splitRange(range: Range, size: number): Range[] {
+    const regions: Range[] = [];
+    for (let i = Math.floor(range.start / size) * size; i < Math.ceil(range.end / size) * size; i += size) {
+        regions.push({ chromosome: range.chromosome, start: i, end: i + size });
+    }
+    return regions;
+}
+const useAsync = <Data>(asyncFunction: () => Promise<Data>, immediate = true) => {
+    const [status, setStatus] = useState<'idle' | 'pending' | 'success' | 'error'>('idle');
+    const [value, setValue] = useState<Data | null>(null);
+    const [error, setError] = useState<Error | null>(null);
+
+    // The execute function wraps asyncFunction and
+    // handles setting state for pending, value, and error.
+    // useCallback ensures the below useEffect is not called
+    // on every render, but only if asyncFunction changes.
+    const execute = useCallback(() => {
+        setStatus('pending');
+        setValue(null);
+        setError(null);
+
+        return asyncFunction()
+            .then(response => {
+                setValue(response);
+                setStatus('success');
+            })
+            .catch(error => {
+                setError(error);
+                setStatus('error');
+            });
+    }, [asyncFunction]);
+
+    // Call execute if we want to fire it right away.
+    // Otherwise execute can be called later, such as
+    // in an onClick handler.
+    useEffect(() => {
+        if (immediate) {
+            execute();
+        }
+    }, [execute, immediate]);
+
+    return { execute, status, value, error };
+};
+
+
+type MotifsResult = {
+    status: 'pending' | 'error' | 'success';
+    data: MotifQueryDataOccurrence[] | null;
+    error: Error | null;
+};
+export function useMotifsInPeak(genomic_region: Range): MotifsResult {
+    const client = (useContext(ApiContext) as any).client as any;
+    
+
+    const queries = useCallback(
+        () =>
+            Promise.all(
+                splitRange(genomic_region, 200).map((genomic_region: any) =>
+                    client.query({
+                        query: MOTIFS_QUERY,
+                        variables: { genomic_region },
+                    }) as any
+                )
+            ),
+        // eslint-disable-next-line
+        [client, genomic_region.chromosome, genomic_region.start, genomic_region.end]
+    );
+    const { error, value } = useAsync(queries);
+
+    if (error !== null) {
+        return { status: 'error', error, data: null };
+    }
+
+    const firstError = value?.find(v => !!v.error);
+    if (!!firstError) {
+        const error = firstError.error!!;
+        return { status: 'error', error, data: null };
+    }
+
+    const allData = value?.flatMap(v => v.data.meme_occurrences);
+    if (!allData) {
+        return { status: 'pending', error: null, data: null };
+    }
+
+    const overlaps = (first: Range, second: Range) => second.start < first.end && second.end > first.start;
+    const occurrences = allData
+        .filter(occurrence => !!occurrence.motif) // Currently, metadata is missing which makes motifs be null
+        .filter(occurrence => overlaps(genomic_region, occurrence.genomic_region))
+        .filter(occurrence => {
+            const poorCentrality = occurrence.motif.flank_z_score < 0 || occurrence.motif.flank_p_value > 0.05;
+            const poorPeakEnrichment =
+                occurrence.motif.shuffled_z_score < 0 || occurrence.motif.shuffled_p_value > 0.05;
+            return !poorCentrality && !poorPeakEnrichment;
+        });
+
+    return { status: 'success', error: null, data: occurrences };
+}
