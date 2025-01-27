81e946b8502c623071c9a73ca1d44433008b9202
mlacadie <lacadiem14@gmail.com>
2025-01-27 13:39:28 -0500

    closable drawer

--- src/components/motifsearch/motiffileuploaddrawer.tsx
+++ src/components/motifsearch/motiffileuploaddrawer.tsx
@@ -6,9 +6,11 @@ import {
     List,
     ListItem,
     ListItemText,
+    Stack,
     TextField,
+    Tooltip,
 } from "@mui/material";
-import { Clear } from "@mui/icons-material";
+import { Clear, ArrowBackIosNewSharp, ArrowForwardIos } from "@mui/icons-material";
 import { Motif } from "@/app/motif/human/meme-search/types";
 
 const MotifDrawer = ({
@@ -21,6 +23,7 @@ const MotifDrawer = ({
     onMotifSelect: (motif: Motif) => void;
 }) => {
     const [searchTerm, setSearchTerm] = useState("");
+    const [drawerOpen, setDrawerOpen] = useState(true)
 
     // Filter motifs based on the search query
     const filteredMotifs = motifs.filter((motif) =>
@@ -30,47 +33,70 @@ const MotifDrawer = ({
     return (
         <Box
             sx={{
-                width: { xs: "100%", sm: "100%", md: "20%" },
+                width: drawerOpen ? { xs: "100%", sm: "100%", md: "20%" } : "0%",
                 height: "100vh", // Respect header/footer
                 position: "relative", // Not fixed, part of the layout
                 overflowY: "auto", // Allow scrolling of drawer content
-                paddingRight: { md: "10px" },
                 backgroundColor: "white",
                 borderRight: "1px solid #ccc",
+                transition: "width 0.3s ease", // Smooth transition when opening/closing
             }}
         >
-            {/* Search Bar */}
+            {!drawerOpen && (
+                <Tooltip title="Open Experiment Selection" placement="right">
+                    <IconButton
+                        onClick={() => setDrawerOpen(true)}
+                        sx={{
+                            position: "fixed",
+                            top: "50%", // Center the button vertically
+                            left: 5,
+                            transform: "translateY(-50%)", // Adjust centering
+                            zIndex: 2000,
+                            backgroundColor: "white",
+                            boxShadow: 3,
+                        }}
+                        color="primary"
+                    >
+                        <ArrowForwardIos />
+                    </IconButton>
+                </Tooltip>
+            )}
             <Box sx={{ padding: "16px", borderBottom: "1px solid #ccc" }}>
-                <TextField
-                    label="Search Motifs"
-                    variant="outlined"
-                    fullWidth
-                    size="small"
-                    value={searchTerm}
-                    onChange={(e) => setSearchTerm(e.target.value)}
-                    InputProps={{
-                        endAdornment: searchTerm && (
-                            <InputAdornment position="end">
-                                <IconButton onClick={() => setSearchTerm("")}>
-                                    <Clear />
-                                </IconButton>
-                            </InputAdornment>
-                        ),
-                        sx: {
-                            backgroundColor: "rgba(129, 105, 191, 0.09)",
-                            borderRadius: "50px",
-                            paddingLeft: "20px",
-                        },
-                    }}
-                    sx={{
-                        "& .MuiOutlinedInput-root": {
-                            "& fieldset": { borderColor: "#8169BF" },
-                        },
-                    }}
-                />
+                <Stack direction="row" p={1} gap={0.5}>
+                    <TextField
+                        label="Search Motifs"
+                        variant="outlined"
+                        fullWidth
+                        size="small"
+                        value={searchTerm}
+                        onChange={(e) => setSearchTerm(e.target.value)}
+                        InputProps={{
+                            endAdornment: searchTerm && (
+                                <InputAdornment position="end">
+                                    <IconButton onClick={() => setSearchTerm("")}>
+                                        <Clear />
+                                    </IconButton>
+                                </InputAdornment>
+                            ),
+                            sx: {
+                                backgroundColor: "rgba(129, 105, 191, 0.09)",
+                                borderRadius: "50px",
+                                paddingLeft: "20px",
+                            },
+                        }}
+                        sx={{
+                            "& .MuiOutlinedInput-root": {
+                                "& fieldset": { borderColor: "#8169BF" },
+                            },
+                        }}
+                    />
+                    <Tooltip title="Collapse Motif Selection" placement="top">
+                        <IconButton onClick={() => setDrawerOpen(false)} color="primary">
+                            <ArrowBackIosNewSharp />
+                        </IconButton>
+                    </Tooltip>
+                </Stack>
             </Box>
-
-            {/* Motif List */}
             <List disablePadding>
                 {filteredMotifs.map((motif) => (
                     <ListItem
