b37e43b172844c282b154679760079d0bd28dcb6
jpfisher72 <jpfisher72@gmail.com>
2025-01-31 11:17:54 -0500

    Fix #108

--- src/app/downloads/page.tsx
+++ src/app/downloads/page.tsx
@@ -14,6 +14,7 @@ import {
   Stack,
   Divider,
   Alert,
+  Tooltip,
 } from "@mui/material";
 import SaveAltIcon from "@mui/icons-material/SaveAlt";
 import InfoIcon from "@mui/icons-material/Info";
@@ -108,7 +109,15 @@ const DownloadPage: React.FC = () => {
             <Stack spacing={2}>
               <Typography variant="h6">
                 MEME ChIP-seq Catalog
-                <InfoIcon sx={{ ml: 1 }} />
+                <Tooltip
+                  title={`
+                    These motifs were identified by applying MEME to the top 500 IDR thresholded ChIP-seq peaks from more than 3,000 ENCODE
+                    ChIP-seq experiments. Five motifs were identified per experiment; these were subsequently filtered for quality using peak
+                    centrality and enrichment metrics
+                  `}
+                >
+                  <InfoIcon sx={{ ml: 1 }} />
+                </Tooltip>
               </Typography>
               <Stack direction={"row"} spacing={2}>
                 <Typography variant="body2">
@@ -146,7 +155,15 @@ const DownloadPage: React.FC = () => {
             <Stack spacing={2}>
               <Typography variant="h6">
                 HT-SELEX Catalog
-                <InfoIcon sx={{ ml: 1 }} />
+                <Tooltip
+                  title={`
+                    These motifs were identified by applying the ZMotif neural network to reads from HT-SELEX experiments and negative reads generated
+                    by dinucleotide shuffling of true positive reads. The motif identified by the network as most predictive of positive reads for each
+                    experiment is contained in this set. HDF5 format motifs can be loaded into Python for application in new models.
+                  `}
+                >
+                  <InfoIcon sx={{ ml: 1 }} />
+                </Tooltip>
               </Typography>
               <Stack direction={"row"} spacing={2}>
                 <Typography variant="body2">
