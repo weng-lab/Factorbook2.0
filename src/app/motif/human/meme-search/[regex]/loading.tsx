1a829b1c4274b70a3eabde3422092cfdd451086d
mlacadie <lacadiem14@gmail.com>
2025-02-06 19:44:16 -0500

    motif loading skeletons

---
+++ src/app/motif/human/meme-search/[regex]/loading.tsx
@@ -0,0 +1,9 @@
+import { Box, Skeleton } from "@mui/material";
+
+export default function LoadingMotifSearch() {
+    return (
+        <Box width={"100vw"} height={"100vh"}>
+            <Skeleton width={"90%"} height={"90%"}/>
+        </Box>
+    );
+}
